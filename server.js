8059b784d4024d246108f3706e45438a4e0b1a67
James Fisher <jfisher396@hotmail.com>
2020-11-25 18:56:01 -0800

    reserve and tables page files created

--- server.js
+++ server.js
@@ -7,11 +7,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//public routes
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//page routes
 app.get("/", function(req,res) {
     res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get("/reserve", function(req,res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+})
+
+app.get("/tables", function(req,res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 })
 
 
