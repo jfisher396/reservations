ec1f71176dc63102e64a9317a31252d46bab445b
James Fisher <jfisher396@hotmail.com>
2021-01-23 11:46:18 -0800

    created onclick function to grab form info

--- public/script.js
+++ public/script.js
@@ -1,2 +1,17 @@
 console.log('public/script.js is working');
+$(document).ready(function() {
 
+    $('#add-res').on("click", function() {
+        
+        let resName = $('#res-name').val();
+        let partySize = $('#party-size').val();
+        let phone = $('#phone').val();
+        console.log(resName,partySize,phone)
+    })
+
+
+
+
+
+
+})
