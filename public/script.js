2be2686503e823430740bf2f3875384d43aa6687
James Fisher <jfisher396@hotmail.com>
2021-01-23 12:25:17 -0800

    notes added to script.js file

--- public/script.js
+++ public/script.js
@@ -1,67 +1,54 @@
-console.log('public/script.js is working');
-$(document).ready(function() {
+console.log("public/script.js is working");
+$(document).ready(function () {
+  //retrieves table data from the API and creates a card for each party
+  $.get("/api/tables").then((data) => {
+    const tableDiv = $("#tables");
 
-    $.get("/api/tables").then((data) => {
-        const tableDiv = $("#tables");
+    data.forEach((table) => {
+      let tableCard = $("<div>");
+      tableCard.addClass("card");
+      tableCard.addClass("table-card");
+      tableCard.html(
+        `<h4>Name: ${table.name}</h4><p>Party Size: ${table.partySize}</p><p>Phone: ${table.phone}</p>`
+      );
+      tableDiv.append(tableCard);
+    });
+  });
+  // retrieves waitlist data from API and creates a card for each party
+  $.get("/api/waitlist").then((data) => {
+    const waitDiv = $("#waiting");
 
-        data.forEach((table) => {
-          let tableCard = $("<div>");
-          tableCard.addClass("card");
-          tableCard.addClass("table-card");
-          tableCard.html(
-            `<h4>Name: ${table.name}</h4><p>Party Size: ${table.partySize}</p><p>Phone: ${table.phone}</p>`
-          );
-          tableDiv.append(tableCard);
-        });
+    data.forEach((table) => {
+      let waitCard = $("<div>");
+      waitCard.addClass("card");
+      waitCard.addClass("waitlist-card");
+      waitCard.html(
+        `<h4>Name: ${table.name}</h4><p>Party Size: ${table.partySize}</p><p>Phone: ${table.phone}</p>`
+      );
+      waitDiv.append(waitCard);
+    });
+  });
 
-        // console.log("table data:", data)
-      });
-      $.get("/api/waitlist").then((data) => {
+  // creates a new reservation based on user input and either adds it to the table list or waitlist depending on how many tables are left
+  $("#add-res").on("click", function (event) {
+    const newReservation = {
+      name: $("#res-name").val().trim(),
+      partySize: $("#party-size").val().trim(),
+      phone: $("#phone").val().trim(),
+    };
 
-        const waitDiv = $("#waiting");
-
-        data.forEach((table) => {
-          let waitCard = $("<div>");
-          waitCard.addClass("card");
-          waitCard.addClass("waitlist-card");
-          waitCard.html(
-            `<h4>Name: ${table.name}</h4><p>Party Size: ${table.partySize}</p><p>Phone: ${table.phone}</p>`
-          );
-          waitDiv.append(waitCard);
-        });
-      });
-
-
-    $('#add-res').on("click", function(event) {
-
-        event.preventDefault();
-
-        const newReservation = {
-            name: $('#res-name').val().trim(),
-            partySize: $('#party-size').val().trim(),
-            phone: $('#phone').val().trim()
-        }
-        
-        // console.log(newReservation)
-
-        $.post("api/tables", newReservation, function(data) {
-            
-            if (data.hasTable) {
-                alert(`A table for ${data.name} has been reserved for ${data.partySize}.`)
-            } else {
-                alert(`${data.name} has been put onto the waitlist.`)
-            }
-
-            $('#res-name').val("");
-            $('#party-size').val("1");
-            $('#phone').val("");
-        })
-
-    })
-
-
-
-
-
-
-})
+    $.post("api/tables", newReservation, function (data) {
+      if (data.hasTable) {
+        alert(
+          `A table for ${data.name} has been reserved for ${data.partySize}.`
+        );
+      } else {
+        alert(`${data.name} has been put onto the waitlist.`);
+      }
+      // clears out the form upon submit
+      $("#res-name").val("");
+      $("#party-size").val("1");
+      $("#phone").val("");
+    });
+  });
+});
