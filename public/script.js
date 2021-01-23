e4669e174fe5cf8d370b196ce9fda4cba12f8e86
James Fisher <jfisher396@hotmail.com>
2021-01-23 12:19:09 -0800

    all js moved to script.js file in public folder

--- public/script.js
+++ public/script.js
@@ -1,6 +1,37 @@
 console.log('public/script.js is working');
 $(document).ready(function() {
 
+    $.get("/api/tables").then((data) => {
+        const tableDiv = $("#tables");
+
+        data.forEach((table) => {
+          let tableCard = $("<div>");
+          tableCard.addClass("card");
+          tableCard.addClass("table-card");
+          tableCard.html(
+            `<h4>Name: ${table.name}</h4><p>Party Size: ${table.partySize}</p><p>Phone: ${table.phone}</p>`
+          );
+          tableDiv.append(tableCard);
+        });
+
+        // console.log("table data:", data)
+      });
+      $.get("/api/waitlist").then((data) => {
+
+        const waitDiv = $("#waiting");
+
+        data.forEach((table) => {
+          let waitCard = $("<div>");
+          waitCard.addClass("card");
+          waitCard.addClass("waitlist-card");
+          waitCard.html(
+            `<h4>Name: ${table.name}</h4><p>Party Size: ${table.partySize}</p><p>Phone: ${table.phone}</p>`
+          );
+          waitDiv.append(waitCard);
+        });
+      });
+
+
     $('#add-res').on("click", function(event) {
 
         event.preventDefault();
