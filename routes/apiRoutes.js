49c3c21bae16933b0611c0a6d9f162a6ff709785
James Fisher <jfisher396@hotmail.com>
2021-01-25 14:19:31 -0800

    delete route created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,26 +28,51 @@ module.exports = function (app) {
     if (tables.length < 5) {
       tables.push(newRes);
       newRes.hasTable = true;
-      console.log(tables);
-      writeFileAsync(__dirname + "/../db/tables.json", JSON.stringify(tables, null, "\t")).then(function (err, data) {
+
+      writeFileAsync(__dirname + "/../db/tables.json", JSON.stringify(tables, null, "\t")).then(() => {
           console.log("new party added to tables");
       }).catch(function(err){
         throw err
       });
 
     } else {
-      waitlist.push(req.body);
+      waitlist.push(newRes);
       newRes.hasTable = false;
 
-      writeFileAsync(__dirname + "/../db/waitlist.json", JSON.stringify(waitlist, null, "\t"), function (err, data) {
-          if (err) {
-            return console.log(err);
-          }
+      writeFileAsync(__dirname + "/../db/waitlist.json", JSON.stringify(waitlist, null, "\t")).then(() => {
           console.log("party added to waitlist");
-        }
-      );
+      }).catch(function(err){
+        throw err
+      });
     }
 
     res.json(newRes);
   });
+
+  // app.put("/api/waitlist/:id", function(req,res) {
+  //   console.log(req.body.id)
+  //   res.send(`Got a PUT request at ${req.body.id}`)
+  // })
+  // route to remove a table from the tables list
+  app.delete("/api/tables/:id", function(req,res) {
+    
+    const tableToDelete = req.params.id;
+    const updatedTables = [];
+
+    for (let i = 0; i < tables.length; i++) {
+        if (tableToDelete !== tables[i].id) {
+            updatedTables.push(tables[i])
+        }
+    }
+
+    fs.writeFile(__dirname + "/../db/tables.json", JSON.stringify(updatedTables, null, "\t"), function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("table deleted");
+      }
+    );
+
+      return res.send(updatedTables)
+  })
 };
